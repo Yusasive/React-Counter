90a66122f0e27b0d7bd2ea62a5a810b4c89ec7c3
Yusasive <74152230+Yusasive@users.noreply.github.com>
2022-10-10 23:57:23 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './style.css';
+
+class Counter extends React.Component {
+  state = {
+    counter: 0
+  }
+  increment = () => {
+    this.setState({counter: this.state.counter+1});
+  }
+  render() {
+    return <div>
+    <p>{this.state.counter}</p>
+    <button onClick={this.increment}>Increment</button>
+    </div>;
+  }
+}
+
+const el = <Counter />; 
+ReactDOM.render(
+  el, 
+  document.getElementById('root')
+);
+export default Counter;
